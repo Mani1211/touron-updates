456d160b121e4a36da9c642f689c0cd864530194
Mani1211 <smainvasagamtnj@gmail.com>
2020-06-05 10:29:47 +0530

    File structure changed

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function(){
+  
+
+  $('.responsive').slick({
+    dots: true,
+    customPaging: function(slider, i) {
+      // this example would render "tabs" with titles
+      return '<span class="active dot far fa-circle"></span>';
+    },
+
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows:true,
+    prevArrow: '<div class="left"><span class="fas fa-arrow-circle-left"></span><span class="sr-only">Prev</span></div>',
+    nextArrow: '<div class="right"><span class="fas fa-arrow-circle-right"></span><span class="sr-only">Next</span></div>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+      
+  });
